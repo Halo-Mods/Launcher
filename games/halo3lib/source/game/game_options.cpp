5f66c76c5157ab10e817792473963e3aa6e9e125
0xfedfedfe <70035962+0xfedfedfe@users.noreply.github.com>
2023-02-16 05:26:49 +0000

    rename to g_game_tick_rate_override

--- games/halo3lib/source/game/game_options.cpp
+++ games/halo3lib/source/game/game_options.cpp
@@ -1,10 +1,10 @@
 #include "halo3lib_private_pch.h"
 
 HALO3_DECLARE_HOOK(0x1800E4058, game_options_new);
-HALO3_DECLARE_REFERENCE(0x180B0251A, uint8_t, g_game_tickrate_override);
+HALO3_DECLARE_REFERENCE(0x180B0251A, uint8_t, g_game_tick_rate_override);
 
 void __cdecl game_options_new(struct game_options* options)
 {
-	g_game_tickrate_override = 30;
+	g_game_tick_rate_override = 30;
 	INVOKE_HOOK_ORIGINAL(game_options_new, options);
 }
